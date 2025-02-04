8eb7e738c68f43c37b05b177fa2d47437ff85e66
Brennan Sellheim <dw1685ao@mnstate.edu>
2025-02-04 11:47:31 -0600

    Update Lab 4

--- Lab 4.R
+++ Lab 4.R
@@ -1,8 +1,10 @@
-library(tidyverse) + library(readr) + library(readxl) + library(googlesheets4) + library(tibble)
-read_csv("1972_2011_sex_age_composition_dwc.csv")
-
-read_excel("Means_of_reservoir_nutrients.xlsx")
-
+library(tidyverse)
+library(readr) 
+library(readxl) 
+library(googlesheets4) 
+library(tibble)
+read_csv("1972_2011_sex_age_composition_dwc.csv", na = "-999")
+read_excel("Means_of_reservoir_nutrients.xlsx", na = "na")
 gs4_deauth()
 read_sheet("15fozF3WTnH-PIL3Qm6RIyOFM6yk0RlxO3lBPF1ndu9s")
 tibble("Cause" = c("Heart Disease", "Cancer", "COVID-19"), "Deaths" = c("695547", "605213", "416893"), 
